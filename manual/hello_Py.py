4a8519ed1635785fe6a5a6ea24c79747ce3f0222
Mohammad Sayem <sayem2@ascend-rw02.ten.osc.edu>
2026-07-05 19:19:14 -0400

    first commit containing the manual portion of assignment 1

---
+++ manual/hello_Py.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Mohammad's Hello World Python Notebook
+
+# ## This is my first Jupyter Notebook
+
+# Just doing my *assignment*
+
+
+print("Hello World")
